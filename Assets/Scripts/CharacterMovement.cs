fddfc2859e8b53d2e99aa9263c2e103c151ce885
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-09 12:37:55 -0400

    Add support for slower turns in locomotion blend

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -15,20 +15,27 @@ public class CharacterMovement : MonoBehaviour
 
     public void Update()
     {
-        DetermineSpeed();
+        DetermineMoveSpeed();
         var vertical = Input.GetAxis("Vertical");
         var horizontal = Input.GetAxis("Horizontal");
+        var halfHorizontal = Input.GetAxis("HalfHorizontal");
+
+        var turnSpeed = DecideTurnSpeed(horizontal, halfHorizontal);
 
         var isMoving = vertical > 0;
         anim.SetBool("IsMoving", isMoving);
-        anim.SetFloat("TurnSpeed", horizontal);
+        anim.SetFloat("TurnSpeed", turnSpeed);
 
         anim.SetFloat("MoveSpeed", moveSpeed);
     }
 
-    private void DetermineSpeed()
+    private void DetermineMoveSpeed()
     {
-        KeyCode[] speedKeys = {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0};
+        KeyCode[] speedKeys =
+        {
+            KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5,
+            KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0
+        };
         var speed = moveSpeed;
 
         for (var i = 0; i < speedKeys.Length; i++)
@@ -41,5 +48,21 @@ public class CharacterMovement : MonoBehaviour
             }
         }
         moveSpeed = speed;
+    }
+
+    private static float DecideTurnSpeed(float horizontal, float halfHorizontal)
+    {
+        float turnSpeed = 0;
+
+        if (horizontal != 0)
+        {
+            turnSpeed = horizontal;
+        }
+        else if(halfHorizontal != 0)
+        {
+            turnSpeed = halfHorizontal * 0.5f;
+        }
+
+        return turnSpeed;
     }
 }
