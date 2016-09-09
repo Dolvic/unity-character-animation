e9bf09684ff71631d0c848de0f73b40957c8db7a
Gupta, Akshay <akshay_gupta@gatech.edu>
2016-09-09 11:49:52 -0400

    Implement forward motion with varying speeds

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Animator anim;
+    private float moveSpeed;
+
+    public void Awake()
+    {
+        anim = GetComponent<Animator>();
+        moveSpeed = 0;
+    }
+
+    public void Update()
+    {
+        SetSpeed();
+        var vertical = Input.GetAxis("Vertical");
+
+        var isMoving = vertical > 0;
+        anim.SetBool("IsMoving", isMoving);
+        anim.SetFloat("MoveSpeed", moveSpeed);
+    }
+
+    private void SetSpeed()
+    {
+        KeyCode[] speedKeys = {KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9, KeyCode.Alpha0};
+        var speed = moveSpeed;
+
+        for (var i = 0; i < speedKeys.Length; i++)
+        {
+            var speedKey = speedKeys[i];
+            if (Input.GetKeyUp(speedKey))
+            {
+                speed = (float) Math.Round(i / 9.0, 2);
+                break;
+            }
+        }
+        moveSpeed = speed;
+    }
+}
